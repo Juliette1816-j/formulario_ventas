35d797ad74c0c7d5aba9d217d59166d3b717e35b
Juliette1816-j <140426982+Juliette1816-j@users.noreply.github.com>
2026-06-23 23:56:50 -0500

    Crear login.js

---
+++ login.js
@@ -0,0 +1,26 @@
+import { supabase } from './supabase.js'
+
+document
+.getElementById('btnLogin')
+.addEventListener('click', async () => {
+
+    const email =
+    document.getElementById('email').value
+
+    const password =
+    document.getElementById('password').value
+
+    const { error } =
+    await supabase.auth.signInWithPassword({
+        email,
+        password
+    })
+
+    if(error){
+        alert(error.message)
+        return
+    }
+
+    window.location =
+    'dashboard.html'
+})
