d1d7b080da0f888ca0419c0649ee2a2c7825d6a0
Juliette1816-j <140426982+Juliette1816-j@users.noreply.github.com>
2026-06-23 21:20:31 -0500

    Create conexion_BD.js

---
+++ conexion_BD.js
@@ -0,0 +1,8 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+const supabase = createClient(
+  "https://jzuxaxlnguvsvlmymkge.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imp6dXhheGxuZ3V2c3ZsbXlta2dlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIyNTI2NjIsImV4cCI6MjA5NzgyODY2Mn0.DatIvM5O6mFz0qhR8tRreB0TCyB8pBMj5FBo0GmMEQo"
+);
+
+const supabase = createClient(supabaseUrl, supabaseKey);
